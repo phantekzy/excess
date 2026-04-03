120f256804c683597662669b39ba55f3bde38416
phantekzy <mainilotfi@gmail.com>
2026-04-03 19:34:48 +0100

    The Stock Keeping Unit

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -3,4 +3,5 @@ import sequelize from "../config/database";
 
 export const Product = sequelize.define("product", {
   id: { type: DataTypes.UUID, defaultValue: UUIDV4, primaryKey: true },
+  sku: { type: DataTypes.STRING, unique: true },
 });
