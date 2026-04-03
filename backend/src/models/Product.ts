d37477d718caf02211313159b61e568b23057d69
phantekzy <mainilotfi@gmail.com>
2026-04-03 19:38:57 +0100

    Product table is done

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,8 +1,12 @@
-import { DataTypes, UUIDV4 } from "sequelize";
+import { DataTypes } from "sequelize";
 import sequelize from "../config/database";
 
 export const Product = sequelize.define("product", {
-  id: { type: DataTypes.UUID, defaultValue: UUIDV4, primaryKey: true },
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
   sku: { type: DataTypes.STRING, unique: true },
   name: { type: DataTypes.STRING, allowNull: false },
   quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
