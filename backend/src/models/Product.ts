005bb79c6f1c514723f0551992d1ae22e5c1e89e
phantekzy <mainilotfi@gmail.com>
2026-04-03 19:38:12 +0100

    Price

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -6,4 +6,5 @@ export const Product = sequelize.define("product", {
   sku: { type: DataTypes.STRING, unique: true },
   name: { type: DataTypes.STRING, allowNull: false },
   quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
+  price: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.0 },
 });
