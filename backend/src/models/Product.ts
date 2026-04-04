ef8d812a387c8dddda69f6f60d46955d175691b9
phantekzy <mainilotfi@gmail.com>
2026-04-04 20:16:14 +0100

    this format to save is going to make me crazy

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -10,5 +10,5 @@ export const Product = sequelize.define("product", {
   sku: { type: DataTypes.STRING, unique: true },
   name: { type: DataTypes.STRING, allowNull: false },
   quantity: { type: DataTypes.INTEGER, defaultValue: 0 },
-  price: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.0 },
+  price: { type: DataTypes.DECIMAL(10, 2), defaultValue: 0.00 },
 });
