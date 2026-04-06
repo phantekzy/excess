178d1ad1423bbc35b5e87bc2e87e619c480bd31a
phantekzy <mainilotfi@gmail.com>
2026-04-06 19:34:30 +0100

    Inventory service is done , added the adjust stuck and i added returning idk if i keep it i added it last second because i said i wont to get the data back

--- backend/src/services/inventoryService.ts
+++ backend/src/services/inventoryService.ts
@@ -1,5 +1,17 @@
-import { Product } from "../models/Product"
+import { where } from "sequelize";
+import { Product } from "../models/Product";
 
-export const fetchAllProducts = async() =>{
-    return await Product.findAll();
-}
+export const fetchAllProducts = async () => {
+  return await Product.findAll();
+};
+
+export const adjustStock = async (id: string, quantity: number) => {
+  const [affectedCount, updatedProducts] = await Product.update(
+    { quantity },
+    {
+      where: { id },
+      returning: true,
+    },
+  );
+  return updatedProducts[0];
+};
