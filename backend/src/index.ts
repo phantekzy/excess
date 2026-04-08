b044d37ac77f72c233b38237b0a95663bddf99b0
phantekzy <mainilotfi@gmail.com>
2026-04-08 07:19:18 +0100

    Entry point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,9 +2,23 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import inventoryRoutes from "./routes/inventoryRoutes";
+import sequelize from "./config/database";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use("api/inventory", inventoryRoutes);
+app.use("/api/inventory", inventoryRoutes);
+
+const start = async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    app.listen(PORT, () => {
+      console.log(`Excess server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
